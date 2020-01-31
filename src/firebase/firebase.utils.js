324ec39121c771cdbe84f413d600ff9952fc9901
Jihtak <matteo.milanovic@gmail.com>
2020-01-31 07:15:24 +0100

    implement ability to store users to firestore database and add email and password signup method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,28 @@ const config = {
 	measurementId: "G-Y8CTQRG847"
 };
 
+export const createUserProfileDocument = async (userAuth, additionaData) => {
+	if (!userAuth) return;
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionaData
+			});
+		} catch (err) {
+			console.log("error creating user", err.message);
+		}
+	}
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
